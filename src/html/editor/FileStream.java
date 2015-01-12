692c3703e5ec1c43b499e9cec6fee627bcaf002b
JindoKuyo <123itvn@gmail.com>
2015-01-13 02:16:45 +0700

    HTML Editor

---
+++ src/html/editor/FileStream.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package html.editor;
+
+import java.io.File;
+
+/**
+ *
+ * @author TOAN
+ */
+public class FileStream {
+    public static File readFile(String file){
+        File fileName = new File(file);
+        
+        
+        return fileName;
+    }
+}
